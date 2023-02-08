beb5c5ad8aeba50194ad242d332fd3d7ca24ed3b
X_V.neer <xv.neer@gmail.com>
2023-02-08 17:02:37 +0200

    created log function

---
+++ src/LogEvent.ts
@@ -0,0 +1,30 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import fsPromises from "node:fs/promises";
+import fs from "node:fs";
+import path from "node:path";
+import { v4 as uuid } from "uuid";
+import { format } from "date-fns";
+import { logFun } from "./types/index.js";
+
+const LogEvent: logFun = async (url, message) => {
+  const dateTime = `${format(new Date(), "yyyy-MMM-do")}`;
+  const logItem = `${dateTime}\t ${uuid()}\t ${url}\t ${message}\n`;
+  const logsDir = path.join(__dirname, "logs");
+  try {
+    if (!fs.existsSync(logsDir)) await fsPromises.mkdir(logsDir);
+    fsPromises.appendFile(path.join(logsDir, "eventlogs.txt"), logItem);
+  } catch (err: any) {
+    // console.log(err);
+    if (!fs.existsSync(logsDir)) await fsPromises.mkdir(logsDir);
+    fsPromises.appendFile(
+      path.join(logsDir, "ErrorsEventlogs.txt"),
+      err?.message
+    );
+  }
+};
+
+export default LogEvent;
